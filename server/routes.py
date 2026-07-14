2677c1d77b692a6bb31269c36d655891fa203486
Abin john <156599811+abinjohn-art@users.noreply.github.com>
2026-07-14 19:21:59 +0530

    Refactor SQL query to use consistent indentation

--- server/routes.py
+++ server/routes.py
@@ -10,11 +10,11 @@ def index():
     name = request.args.get('name')
     author = request.args.get('author')
     read = bool(request.args.get('read'))
-
-    if name:
-        cursor.execute(
-            "SELECT * FROM books WHERE name LIKE '%" , name + "%'"
-        )
+if name:
+    cursor.execute(
+        "SELECT * FROM books WHERE name LIKE '%" + name + "%'"
+    )
+  
         books = [Book(*row) for row in cursor]
 
     elif author:
